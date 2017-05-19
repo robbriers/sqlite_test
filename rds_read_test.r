2f305fde8815fa547e5d44efa5763aa13a74c1c9
Rob Briers <r.briers@napier.ac.uk>
2017-05-19 10:36:07 +0100

    comparing different subsetting options

--- rds_read_test.r
+++ rds_read_test.r
@@ -16,40 +16,26 @@ rivers2<-readRDS(url("https://raw.githubusercontent.com/robbriers/sqlite_test/ma
 rivers3<-readRDS(gzcon(url("https://raw.githubusercontent.com/robbriers/sqlite_test/master/rivers_c.rds")))
 
 # compressed RDS is massively faster to read back in!
-
+# this is now the compressed RDS version - default to be used
 sepa<-readRDS(gzcon(url("https://raw.githubusercontent.com/robbriers/sqlite_test/master/sepa.rds")))
 
-str(sepa)
-
-rivers<-sepa[sepa$Water.body.category=="River", ]
-
-canal<-sepa[sepa$Water.body.ID=="1", ]
-
-# subsetting is still quite slow - try feather format and remote subsetting instead
-
-#library(feather)
-
-#write_feather(sepa, "sepa.feather")
-
-# huge file (174mb - not going to be feasible!)
-
-# try dplyr subsetting instead
-
+# comparing subsetting times
 library(dplyr)
-
-canal2<-filter(sepa, Water.body.ID=="1")
 
 # compare these
 
-system.time(canal2<-filter(sepa, Water.body.ID=="1"))
-system.time(canal<-sepa[sepa$Water.body.ID=="1", ])
+# convert category to character rather than factor
+sepa2<-sepa
+sepa2$Water.body.category<-as.character(sepa2$Water.body.category)
 
-# not much in it!
+# compare different versions
+# base
+system.time(rivers<-sepa[sepa$Water.body.category=="River", ])
+# dplyr
+system.time(rivers2<-filter(sepa, Water.body.category=="River"))
+# character base
+system.time(rivers3<-sepa2[sepa2$Water.body.category=="River", ])
+# character dplyr
+system.time(rivers4<-filter(sepa2, Water.body.category=="River"))
 
-# Could host feather file elsewhere and load/query it from there
-# need to check remote subsetting
-
-test<-read_feather("url")
-canal3<-filter(test, Water.body.ID=="1")
-
-# check relative time for filter by character or factor
+# character dplyr best
